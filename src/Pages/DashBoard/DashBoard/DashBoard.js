776c46c4923907da0ce54ae2647f72052cc9bb5d
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-15 08:34:01 +0600

    dash

--- src/Pages/DashBoard/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard/DashBoard.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import Navbar from '../../../Share/Navbar/Navbar';
-import DashBoardTable from './DashBoardTable';
 
 const DashBoard = () => {
     return (
@@ -9,16 +7,8 @@ const DashBoard = () => {
             <Navbar></Navbar>
             <section class="grid grid-cols-5 gap-4 ">
                 <div class=" text-center">Appointment </div>
-                <div class="col-span-4 bg-slate-200">
-                    <div className='px-20 pt-10'>
-                        <div className='flex justify-between items-center '>
-                            <h1 className='text-2xl font-bold'>My Appointment</h1>
-                            <button className="btn btn-outline">Date</button>
-                        </div>
-                        <div className='mt-10'>
-                            <DashBoardTable></DashBoardTable>
-                        </div>
-                    </div>
+                <div>
+                    hello
                 </div>
             </section>
         </div>
